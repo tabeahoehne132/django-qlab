1c51605c2fed134c043bdfe8dea12c7d857321d2
tabeahoehne132 <tabea.hoehne@outlook.com>
2026-03-10 13:02:43 +0100

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="django-qlab",
-    version="0.1.0",
+    version="0.2.2",
     author="Tabea Hoehne",
     description="Dynamic query API for Django REST Framework with advanced filtering and metadata",
     long_description=long_description,
